0386a37e5a5efe323ffccbd0c77713f08114627d
Bill Phan <billphan25@gmail.com>
2017-05-18 08:24:51 -0700

    Add files via upload

--- factors.py
+++ factors.py
@@ -1,29 +1,29 @@
 # create a function factors that takes in an integer n and generates a dictionary that contains the factors of all values from 1 to n.
 
-# creating a dictionary? 
-factor = {}
-factor['1'] = '1'
-factor['2'] = '1,2'
-factor['3'] = '1,3'
-factor['4'] = '1,2,4'
-
+# creating a function called factors that take n as a parameter
 def factors(n):
-  #start with an empty list
-  factorList = []
-  # for every number in range from 1 to n   
-  for i in range(1, n + 1):
-    # if n is factorable by a number
-    if n % i == 0:
-      # append it onto factorList, else not append
-      factorList.append(i)
-  # return the factored list
-  return factorList
-# test print factor
-print(factors(9))
+    # creating a dictionary
+    factor_dict = {1: [1]}
+    # creating a for loop that takes a range of 2 to n append next integer
+    for num in range(2,n+1):
+        #start with an empty list
+        factor_list = []
+        # add onto factor_list
+        factor_list.append(1)
+        for i in range(2,num):
+            # if the number modulo i is even
+            if num % i == 0:
+                # append it onto list
+                factor_list.append(i)
+        factor_list.append(num)
+        factor_dict[num] = factor_list
+    return factor_dict
 
 # Gather our code in a main() function
 def main():
+    upper_factor_range = input("Enter a number to get the factors of each number up to your number.\n")
+    print(factors(int(upper_factor_range)))
 
 # Standard boilerplate to call the main() function to begin the program.
-  if __name__ == "__main__":
+if __name__ == "__main__":
     main()
